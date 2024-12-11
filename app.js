e0d8f2b3eee069378fb34068a107bb003c0c4b54
epeng590 <59757168+epeng590@users.noreply.github.com>
2024-12-12 10:52:09 +1100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const pokedex = document.getElementById("pokedex")
 
 const getPokemon = () => {
   const promises = []
-  for (let i = 1; i <= 50; i++) {
+  for (let i = 1; i <= 151; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`
     promises.push(fetch(url).then(res => res.json()))
   }
@@ -21,7 +21,7 @@ const displayPokemon = pokemon => {
   const pokemonString = pokemon
     .map(
       singlePokemon => `
-    <li onclick="onClick(${singlePokemon.id})">
+    <li>
       <img src="${singlePokemon.image}" />
       <h3>${singlePokemon.id}. ${singlePokemon.name}</h3>
     </li>`
@@ -30,30 +30,5 @@ const displayPokemon = pokemon => {
   pokedex.innerHTML = pokemonString
 }
 
-const onClick = (id) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      const pokemon = {
-        id: data.id,
-        name: data.name,
-        image: data.sprites["front_default"],
-        height: data.height,
-        weight: data.weight,
-        type: data.types.map(type => type.type.name).join(", "),
-      }
-      displayCard(pokemon)
-    })
-}
-
-const displayCard = pokemon => {
-    document.getElementById("pokecard").innerHTML = `
-    <h2>${pokemon.id}. ${pokemon.name}</h2>
-    <p>Height: ${pokemon.height}</p>
-    <p>Weight: ${pokemon.weight}</p>
-    <p>Type: ${pokemon.type}</p>`
-}
-
 getPokemon()
+
