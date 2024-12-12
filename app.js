a89a1506d2a6c623b5cc32ff17f3e7e49a52ab2e
epeng590 <59757168+epeng590@users.noreply.github.com>
2024-12-12 14:01:26 +1100

    Test local storage

--- app.js
+++ app.js
@@ -32,3 +32,23 @@ const displayPokemon = pokemon => {
 
 getPokemon()
 
+
+let pokemon = ["Pikachu", "Bulbasaur"] // Array of pokemon 
+
+console.log(pokemon)
+
+pokemon.push("Charmander") // add another pokemon "Charmander"
+
+console.log(pokemon)
+
+pokemon = JSON.stringify(pokemon) // convert array of pokemon to String
+
+localStorage.setItem("pokemon", pokemon) // set in local storage
+
+console.log(localStorage.getItem("pokemon")) // returns STRING of pokemons
+console.log(typeof pokemon) // returns String
+
+pokemon = JSON.parse(pokemon) // convert String to Array 
+console.log(typeof pokemon) // returns Object 
+console.log(pokemon) // returns ARRAY of pokemon 
+console.log(pokemon.hasOwnProperty(length))
