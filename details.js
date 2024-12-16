db4de2638b23e2e2dfef27fa670936a05c8634d7
Quyen <188524781+quyenngyn@users.noreply.github.com>
2024-12-16 15:49:07 +1100

    Display pokemon name on detail page

--- details.js
+++ details.js
@@ -4,8 +4,39 @@ const urlParams = new URLSearchParams(queryString); // constructor method - cons
 
 const pokemonID = urlParams.get('pokemonID');
 
-console.log(pokemonID);
-
 const test = document.getElementById("test");
 
-test.textContent = pokemonID;
+const getPokemonStats = () => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonID}`;
+
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow"
+      };
+       
+      fetch(url, requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            const pokemon = {
+                name: result.name,
+                id: result.id
+            }
+        displayStats(pokemon)})
+        .catch((error) => console.error(error));
+}
+
+const displayStats = (pokemon) => {
+    let pokemonString =
+    `<li>${pokemon.name}
+    </li>`;
+    test.innerHTML = pokemonString;
+}
+
+// const pokemonStats = getPokemon();
+
+// const test = document.getElementById("test");
+// console.log(pokemonStats);
+// console.log(pokemonStats.name);
+// test.textContent = pokemonStats.name;
+
+getPokemonStats();
