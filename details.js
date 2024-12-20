7a95773808da7ddc50fc9d65351eccebddae0f9b
epeng590 <59757168+epeng590@users.noreply.github.com>
2024-12-20 13:25:18 +1100

    Fix attributes

--- details.js
+++ details.js
@@ -51,7 +51,7 @@ const displayStats = (pokemon) => {
     img.alt = pokemon.name;
     const statCard = document.getElementById("stat-card");
     const attributesCard = document.getElementById("attributes-card")
-    attributesCard.innerHTML = `<span class="attribute-span"> Height: ${pokemon.height} </span><span class="attribute-span"> Weight: ${pokemon.weight} </span>`;
+    attributesCard.innerHTML = `<span class="attribute-span"> Height: ${(pokemon.height * 0.1).toFixed(1)} m </span><span class="attribute-span"> Weight: ${pokemon.weight * 0.1} kg </span>`;
     const statsItems = document.createElement("p");
     const statsString = pokemon.stats.map((stat) => {
         return `<span class="stat-span"> ${stat.name}: ${stat.baseStat} </span>`;
