dd18db0aed96e56c69a05b8909d84ecc1d1b45db
Saphron H <109723086+denerose@users.noreply.github.com>
2024-12-20 14:24:06 +1100

    Add type colours

--- details.js
+++ details.js
@@ -42,6 +42,7 @@ const displayStats = (pokemon) => {
     pokemon.types.forEach((type) => {
         const li = document.createElement("li");
         li.textContent = type;
+        li.classList.add(type);
         typeList.appendChild(li);
     });
     const img = document.getElementById("card-image");
@@ -56,6 +57,29 @@ const displayStats = (pokemon) => {
     }).join("");
     statsItems.innerHTML = statsString;
     statCard.appendChild(statsItems);
+}
+
+function setMainColor(type) {
+    const colors = {
+        normal: '#A8A77A',
+        fire: '#EE8130',
+        water: '#6390F0',
+        electric: '#F7D02C',
+        grass: '#7AC74C',
+        ice: '#96D9D6',
+        fighting: '#C22E28',
+        poison: '#A33EA1',
+        ground: '#E2BF65',
+        flying: '#A98FF3',
+        psychic: '#F95587',
+        bug: '#A6B91A',
+        rock: '#B6A136',
+        ghost: '#735797',
+        dragon: '#6F35FC',
+        dark: '#705746',
+        steel: '#B7B7CE',
+        fairy: '#D685AD',
+    };
 }
 
 getPokemonStats();
